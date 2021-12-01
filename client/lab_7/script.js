b6ce0c7ecb75e1ad22fb92e8b3fdc434f70511df
Ricky Das <2018rdas@gmail.com>
2021-12-01 03:14:05 -0500

    Lab 7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,22 +1,49 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-var */
 /* eslint-disable indent */
 /* eslint-disable no-shadow */
 /* eslint-disable arrow-body-style */
+
 async function windowActions() {
     const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const request = await fetch(endpoint)
     const places = await request.json()
+    var map = L.map('map').setView([38.989, -76.937], 13);
+    var markers = L.layerGroup().addTo(map);
     function findMatches(word, places) {
       return places.filter((place) => {
         const regex = new RegExp(word, 'gi');
         return place.name.match(regex) || place.category.match(regex)
       });
     }
-  
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoicmlja3lkYXMiLCJhIjoiY2t3bjQwdHU3MGx2dzJvcHI0dWJlcXNxaSJ9.2HAQeRuH6cT5BTqPxQ_yEg'
+    }).addTo(map);
+
+    function compareZip(a, b) {
+        if (Math.abs(a.zip - 20740) < Math.abs(b.zip - 20740)) return -1;
+        if (Math.abs(a.zip - 20740) === Math.abs(b.zip - 20740)) return 0;
+        if (Math.abs(a.zip - 20740) > Math.abs(b.zip - 20740)) return 1;
+    }
+
     function displayMatches(event) {
+      markers.clearLayers();
       const matchArray = findMatches(event.target.value, places);
+      matchArray.sort(compareZip);
+      matchArray.length = 5;
       const html = matchArray.map((place) => {
+        L.marker([place.geocoded_column_1.coordinates[1], place.geocoded_column_1.coordinates[0]]).addTo(markers);
         return `
-        <li style="margin-bottom:20px;">
+        <li class = "box" style="margin-bottom:10px">
           <span class="name">${place.name}</span>
           <br>
           <span class="category">${place.category}</span>
@@ -30,6 +57,11 @@ async function windowActions() {
       `;
       }).join('');
       document.querySelector('.suggestions').innerHTML = html;
+      // eslint-disable-next-line no-use-before-define
+      if (document.querySelector('.search').value.length === 0) { 
+        document.querySelector('.suggestions').innerHTML = '';
+        markers.clearLayers();
+      }
     }
   
     const searchInput = document.querySelector('.search');
